45e777865505821b1588a45bbfe0a31f8161e4b5
vitoremanueldev <veslima3@gmail.com>
2021-09-05 16:10:45 -0300

    Ternary sort

--- Lab 3.b/model/Manipula.java
+++ Lab 3.b/model/Manipula.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public abstract class Manipula {
 
-	int array[] = {5,8,32,67,99,15,12,9,1,99};
+	int array[] = new int [10];
 	int temp = 0;
 
 	public void preencher() {
-		int array[] = new int[10];
+		//int array[] = new int[10];
 		Random aleatorio = new Random();
 		int i = 0;
 		while (i < array.length) {
@@ -18,24 +18,30 @@ public abstract class Manipula {
 		}
 
 	}
+	//teste
+	//int array[] = {5,8,32,67,99,15,12,9,1,99};
 
 	public void ordenar() {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array.length; j++) {
-				if (array[i] < array[j]) {
-					temp = array[i];
-					array[i] = array[j];
-					array[j] = temp;
-				}
+			array[i] = (array[i] < array[j]) ? bubble(i , j): array[i];
 			}
 		}
 	}
 
-	public void imprimir() {
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		public void imprimir() {
+			for (int i = 0; i < array.length; i++) {
+				System.out.println(array[i]);
+			}
 		}
-	}
-	
 
-}
+		public int bubble(int i , int j) {
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			
+			return array[i];
+		}
+
+
+	}
