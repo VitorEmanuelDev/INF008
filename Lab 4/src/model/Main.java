2ae29798952ba6a8b77f7605f2922299434c3074
vitoremanueldev <veslima3@gmail.com>
2021-09-06 02:10:19 -0300

    Lab 4 temperaturas

---
+++ Lab 4/src/model/Main.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException  {
+		 
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        System.out.println("Informe uma temperatura ");
+        int temperatura = scanner.nextInt();
+        System.out.println("Atual tem valor " + temperatura);
+        
+        int quente = 40;
+        int frio = 10;
+        
+        if(temperatura >= quente)
+        	System.out.println("Está quente");
+        else if(temperatura <= frio)
+        	System.out.println("Está frio");
+        else
+        	System.out.println("Normal");
+     
+    }
+
+}
